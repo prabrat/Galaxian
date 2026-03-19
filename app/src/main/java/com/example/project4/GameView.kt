70b369b2e9cdbdad52a24268e5d63ce9c8ef5337
dajanah3 <dajanahlu@gmail.com>
2026-03-19 19:43:28 -0400

    gameview

---
+++ app/src/main/java/com/example/project4/GameView.kt
@@ -0,0 +1,42 @@
+package com.example.project4
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+import androidx.core.graphics.scale
+
+class GameView : View {
+    private lateinit var ship : Bitmap
+    private lateinit var enemy : Bitmap
+    private lateinit var paint : Paint
+    private lateinit var galaxian: Galaxian
+    private var width: Int = 0
+    private var height: Int = 0
+
+    constructor(context: Context, width: Int, height : Int): super(context){
+        paint = Paint()
+        paint.strokeWidth = 20f
+        paint.color = Color.BLACK
+        paint.isAntiAlias = true
+        this.width = width
+        this.height = height
+        val w = 250
+        val h = 250
+        ship = BitmapFactory.decodeResource(resources, R.drawable.ship)
+        ship = ship.scale(w, h)
+        enemy = BitmapFactory.decodeResource(resources, R.drawable.enemy)
+        enemy = enemy.scale(w, h)
+        galaxian = Galaxian(context)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(ship, (width - ship.width) / 2f, 2000f, paint)
+
+
+    }
+}
--- app/src/main/java/com/example/project4/MainActivity.kt
+++ app/src/main/java/com/example/project4/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.project4
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +8,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gameView: GameView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val width : Int = resources.displayMetrics.widthPixels
+        val height : Int = resources.displayMetrics.heightPixels
+
+        gameView = GameView(this, width, height)
+        setContentView(gameView)
     }
 }
